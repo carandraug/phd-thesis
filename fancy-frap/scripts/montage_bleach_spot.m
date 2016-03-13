2a0517d4df9bd4566b15308ce1eec5d95749d652
David Miguel Susano Pinto <david.pinto@nuigalway.ie>
2016-03-13 19:01:39 +0000

    fancy-frap: rename script as montage_bleach_spot as it is the only it will do.
    I'm giving up on tracking the spot automatically, so rename the script to
    the only it managed to do (find the bleach spot), and add it to the helper
    results list of targets.

--- fancy-frap/scripts/montage_bleach_spot.m
+++ fancy-frap/scripts/montage_bleach_spot.m
@@ -1,4 +1,4 @@
-#!/usr/local/bin/octave -qf
+#!/usr/local/bin/octave
 ##
 ## Copyright (C) 2016 Carnë Draug <carandraug+dev@gmail.com>
 ##
@@ -14,10 +14,6 @@
 ##
 ## You should have received a copy of the GNU General Public License
 ## along with this program; if not, see <http://www.gnu.org/licenses/>.
-
-## Do not use this code for anything other than producing the figures for
-## the thesis. The core of this script can be found at:
-## https://github.com/af-lab/scripts/blob/master/microscopy/CropReg.m
 
 pkg load image;
 pkg load statistics;
@@ -43,55 +39,25 @@ function [img] = read_lsm_series (r)
   img = permute (img, [1 2 4 5 3]);
 endfunction
 
-function [img, t] = read_lsm_fancy_frap (pre_fpath, post_fpath, post_series_fpath)
+function [img] = read_lsm_fancy_frap (pre_fpath, post_fpath, post_series_fpath)
 
   [~, ~, ~, series_idx] = regexp (pre_fpath, '(?<=position=)\d+(?=,)');
   series_idx = str2double (series_idx{1});
-  timestamps = {};
 
   ## Read pre bleach
   reader = bfGetReader (pre_fpath);
   pre_img = read_lsm_series (reader);
-  timestamps(end+1) = reader.getMetadataStore ().getImageAcquisitionDate (0).asInstant ();
 
   ## Read post bleach (t = 0)
   reader = bfGetReader (post_fpath);
   post_img = read_lsm_series (reader);
-  timestamps(end+1) = reader.getMetadataStore ().getImageAcquisitionDate (0).asInstant ();
 
   ## Read post bleach time series (t = 1 .. N)
   reader = bfGetReader (post_series_fpath);
   reader.setSeries (series_idx -1);
   post_series_img = read_lsm_series (reader);
-  timestamps(end+1) = reader.getMetadataStore ().getImageAcquisitionDate (0).asInstant ();
 
   img = cat (4, pre_img, post_img, post_series_img);
-
-  ##
-  ## Figure out time intervals
-  ##
-  n_t = size (img, 4);
-  t = zeros (n_t, 1);
-
-  ## For the time difference between pre, post and t1, we don't have time
-  ## interval planes, we only have acquisition time (start of experiment).
-  t(2) = javaObject ("org.joda.time.Duration", timestamps{1},
-                     timestamps{2}).getStandardSeconds ();
-  t(3) = javaObject ("org.joda.time.Duration", timestamps{2},
-                     timestamps{3}).getStandardSeconds ();
-
-  ## Then we have the time delta for each plane since t=1.  Despite the name,
-  ## all planes (all channels and z stacks) for a single point, seem to have
-  ## the same delta T.  But that does make it easier on us.
-  n_planes_by_t = prod (size (img)([3 5]));
-  m = reader.getMetadataStore ();
-  for t_idx = 4:n_t
-    plane_idx = n_planes_by_t * (t_idx - 3);
-    t(t_idx) = m.getPlaneDeltaT (series_idx -1, plane_idx).value ();
-    ## Do not forget to add the time from when we started
-    t(t_idx) += t(3);
-  endfor
-
 endfunction
 
 function bw = im2bw_yen (img)
@@ -213,40 +179,40 @@ function bw_activated = find_bleach_spot (img)
 
 endfunction
 
-function main_montage_log (pre_fpath, post_fpath, series_fpath, log_fpath)
+function [montage_img] = create_montage (activation, bleaching, bw_spot)
 
-  if (nargin != 4)
-    error ("usage: pre_fpath post_fpath series_path log_path");
-  endif
-
-  [img] = read_lsm_fancy_frap (pre_fpath, post_fpath, series_fpath);
-  bw_spot = find_bleach_spot (img);
-  activation = img(:,:,:,1:2,1);
-  bleaching = img(:,:,:,1:2,2);
+  ## Montage size is 5 columns.  Each column is a Z stack of:
+  ##  1) pre bleach of activation channel
+  ##  2) post bleach of activation channel
+  ##  3) mask of identified spot
+  ##  4) pre-bleach of bleaching channel
+  ##  5) post-bleach of bleaching channel
 
   m_log = cat (4, activation, im2uint8 (bw_spot), bleaching);
   m_log = permute (m_log, [1 2 4 3]);
   m_log = reshape (m_log, [size(m_log)(1:2) 1 prod(size(m_log)(3:end))]);
 
   h = figure ("visible", "off");
-  mh = montage (imadjust (m_log, stretchlim (m_log, 0)));
+  mh = montage (imadjust (m_log, stretchlim (m_log, 0)),
+                "Size", [size(bw_spot)(3) 5]);
 
-  imwrite (get (mh, "cdata"), log_fpath);
-
+  montage_img = get (mh, "cdata");
 endfunction
 
-function main (pre_fpath, post_fpath, post_series_fpath)
+function [rv] = main (pre_fpath, post_fpath, series_fpath, montage_fpath)
 
-  if (nargin != 3)
-    error ("usage: pre_fpath post_fpath post_series_path");
+  if (nargin != 4)
+    error ("usage: PRE_FPATH POST_FPATH SERIES_FPATH MONTAGE_FPATH");
   endif
 
-  [img, t_deltas] = read_lsm_fancy_frap (pre_fpath, post_fpath,
-                                         post_series_fpath);
-
+  [img] = read_lsm_fancy_frap (pre_fpath, post_fpath, series_fpath);
   bw_spot = find_bleach_spot (img);
 
+  [montage_img] = create_montage (img(:,:,:,1:2,1), img(:,:,:,1:2,2), bw_spot);
+  imwrite (montage_img, montage_fpath);
+
+  rv = 0;
+  return
 endfunction
 
-#main (argv (){:});
-main_montage_log (argv (){:});
+main (argv (){:});
